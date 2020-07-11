88968ecc8cd06db6337155443c6c16005706f52a
szfh <52595717+szfh@users.noreply.github.com>
2020-07-11 17:45:20 +0100

    knit markdown-test

--- R/knit.R
+++ R/knit.R
@@ -1,1 +1,12 @@
 library(ezknitr)
+library(here)
+
+ls("package:ezknitr")
+# https://github.com/ropensci/ezknitr#ezknitr---avoid-the-typical-working-directory-pain-when-using-knitr
+
+drafts <- here("_drafts")
+posts <- here("_posts")
+tests <- here("_tests")
+fig_dir <- "fig"
+
+ezknit(file=here("R","markdown-test.Rmd"),out_dir=tests,fig_dir=fig_dir,keep_html=FALSE)
