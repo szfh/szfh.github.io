f28893e35ba013e7d0eacaf2f6400a9e0d66f20a
szfh <52595717+szfh@users.noreply.github.com>
2020-07-16 14:35:57 +0100

    fix file name

--- R/knit.R
+++ R/knit.R
@@ -10,4 +10,4 @@ tests <- here("_tests")
 fig_dir <- "fig"
 
 # ezknit(file=here("R","2020-7-11-markdown-test.Rmd"),out_dir=posts,fig_dir=here("images","markdown-test"),keep_html=FALSE)
-# ezknit(file=here("R","2020-7-12-scrape-nicely.Rmd"),out_dir=posts,fig_dir=here("images","scrape-nicely"),keep_html=FALSE)
+ezknit(file=here("R","2020-7-16-cowplot.Rmd"),out_dir=posts,fig_dir=here("images","scrape-nicely"),keep_html=FALSE)
