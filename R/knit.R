b6a6764d230a0550e740c61a4c861e00ba5a318b
szfh <52595717+szfh@users.noreply.github.com>
2020-08-22 13:24:42 +0100

    update print lines

--- R/knit.R
+++ R/knit.R
@@ -12,13 +12,13 @@ ls("package:ezknitr")
 
 knit_to_site <- function(date,name,loc="drafts",keep_html=FALSE){
   file <- here("R",glue("{date}-{name}.Rmd"))
-  print(file)
+  print(glue("file: {file}"))
   
   out_dir <- here(glue("_{loc}"))
-  print(out_dir)
+  print(glue("out_dir: {out_dir}"))
   
   fig_dir <- here("images",glue("{date}-{name}"))
-  print(fig_dir)
+  print(glue("fig_dir: {fig_dir}"))
   
   ezknit(file=file,out_dir=out_dir,fig_dir=fig_dir,keep_html=keep_html)
 }
