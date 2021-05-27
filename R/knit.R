73f8c880e5b66257c7381807cd6d847bfdb2c66d
szfh <52595717+szfh@users.noreply.github.com>
2021-05-27 17:29:23 +0100

    add knit line

--- R/knit.R
+++ R/knit.R
@@ -27,3 +27,4 @@ knit_to_site <- function(date,name,loc="drafts",keep_html=FALSE){
 # knit_to_site("2020-8-2","is-hojbjerg-good","posts")
 # knit_to_site("2020-10-28","fbref-data-viz","posts")
 # knit_to_site("2020-11-19","press-intensity","posts")
+knit_to_site("2021-5-26","are-cup-runs-worth-it","posts")
